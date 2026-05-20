bb2e81c53a628ff2a7fe38efc38e1321201a9089
owaisahmed123 <owais010.com@gmail.com>
2026-05-20 22:21:32 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-    
-  const [apiData, setApiData]  = useState([]);
-  
 
+  const [apiData, setApiData] = useState([]);
 
+  const getData = async () => {
+    const apiresp = await axios.get("https://dummyjson.com/recipes");
+    setApiData(apiresp.data.recipes);
+    // console.log(apiresp.data.recipes);
 
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  useEffect(() => {
+    getData();
+  }, [])
 
 
 
 
   return (
     <div className="App">
+      {apiData.map((eachItem) => {
+        console.log(eachItem)
 
+        return (
+          <div className="div">
+            <img src={eachItem.image} />
+            <h1>{eachItem.name}</h1>
+          </div>
+        );
+      })}
     </div>
   );
-}
 
+}
 export default App;
