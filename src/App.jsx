aa2c1ebb9103b6e3fdd640a6af55291d5388a49d
owaisahmed123 <owais010.com@gmail.com>
2026-05-20 21:46:57 +0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    
+  const [apiData, setApiData]  = useState([]);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
